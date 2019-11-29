cdf45254626d4634367e8da30267e19eb8266c7b
liujie0410 <361454055@qq.com>
2019-11-29 23:59:49 +0800

    add werbwork

---
+++ webwork/out/artifacts/webwork_war_exploded/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liujie
+  Date: 2019-11-09
+  Time: 16:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>HelloWorld</title>
+  </head>
+  <body>
+  </body>
+</html>
---
+++ webwork/src/Servlet.java
@@ -0,0 +1,30 @@
+
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Servlet", urlPatterns = "/Servlet")
+public class Servlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setCharacterEncoding("GBK");
+                ServletContext application =this.getServletContext();
+                 Integer count = (Integer) application.getAttribute("count");
+                 if(count==null){
+                         application.setAttribute("count", 1);
+                     }else{
+                         application.setAttribute("count", count+1);
+                     }
+
+                 // 向浏览器输出，需要使用响应请求
+                 response.setContentType("text/html");
+                 PrintWriter out = response.getWriter();
+                 out.println("<h2>该servlet页面已被访问了 "+count+" 次</h2>");
+    }
+}
